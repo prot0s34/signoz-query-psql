742b7115e51bdf8435f653db268ade62428ffeeb
Yunus M <myounis.ar@live.com>
2024-02-14 23:58:03 +0530

    feat: add sentry package

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import './ReactI18';
 import 'styles.scss';
 
+import * as Sentry from '@sentry/react';
 import AppRoutes from 'AppRoutes';
 import { ThemeProvider } from 'hooks/useDarkMode';
 import ErrorBoundaryFallback from 'pages/ErrorBoundaryFallback/ErrorBoundaryFallback';
@@ -20,6 +21,25 @@ const queryClient = new QueryClient({
 });
 
 const container = document.getElementById('root');
+
+Sentry.init({
+	dsn:
+		'https://4d80b93aec766d983fdf93233cf60fdb@o4506734468530176.ingest.sentry.io/4506734469709824',
+	integrations: [
+		Sentry.browserTracingIntegration(),
+		Sentry.replayIntegration({
+			maskAllText: false,
+			blockAllMedia: false,
+		}),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 if (container) {
 	const root = createRoot(container);
